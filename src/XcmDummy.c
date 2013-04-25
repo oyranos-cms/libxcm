c70787b69b2e25c288e69541e65f04cf2d0e01d3
Kai-Uwe Behrmann <ku.b@gmx.de>
2013-04-25 11:13:19 +0200

    * [core]: call split libraries in libXcm
    ... in order to make automatic dependency resolvers aware of the relation.
    This is useful for RPM package creation.

--- src/XcmDummy.c
+++ src/XcmDummy.c
@@ -1,2 +1,10 @@
+#include "XcmEdidParse.h"
+#include "XcmDDC.h"
+#include "Xcm.h"
+
 void XcmDummy_()
-{}
+{
+  XcmEdidErrorToString(XCM_EDID_OK);
+  XcmDDCErrorToString(XCM_DDC_OK);
+  XcmColorServerCapabilities( NULL );
+}
