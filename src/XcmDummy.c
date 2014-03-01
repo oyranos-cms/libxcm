995ef2dc2ca25d6f435e2e03bbbb129cfbd0635c
Kai-Uwe Behrmann <ku.b@gmx.de>
2014-03-01 15:08:45 +0100

    * [core]: fix conditional compiling of XcmDummy

--- src/XcmDummy.c
+++ src/XcmDummy.c
@@ -5,6 +5,10 @@
 void XcmDummy_()
 {
   XcmEdidErrorToString(XCM_EDID_OK);
+#if defined(HAVE_LINUX)
   XcmDDCErrorToString(XCM_DDC_OK);
+#endif
+#if defined(HAVE_X11)
   XcmColorServerCapabilities( NULL );
+#endif
 }
