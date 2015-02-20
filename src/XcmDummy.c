cec506c484bf17cc91a7a408c7dbec0251f43147
Kai-Uwe Behrmann <ku.b@gmx.de>
2015-02-20 14:45:43 +0100

    * [docu]: explain XcmDummy function

--- src/XcmDummy.c
+++ src/XcmDummy.c
@@ -2,6 +2,9 @@
 #include "XcmDDC.h"
 #include "Xcm.h"
 
+/* This function is used to have at least one symbol in a library, in order
+ * to compile.
+ */
 void XcmDummy_()
 {
   XcmEdidErrorToString(XCM_EDID_OK);
